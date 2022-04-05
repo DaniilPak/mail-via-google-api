11a24cbe42a126a2616b3f49c860c460f1a21693
DaniilPak <1asumeasume1@gmail.com>
2022-04-05 15:19:03 +1000

    first commit

---
+++ pymail.py
@@ -0,0 +1,70 @@
+# Follow guide on https://developers.google.com/gmail/api/quickstart/python to obtain credentials
+
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools, file
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'credentials.json'
+APPLICATION_NAME = 'Gmail API Python Send Email'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'credentials.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def SendMessage(sender, to, subject, msgHtml, msgPlain):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    message1 = CreateMessage(sender, to, subject, msgHtml, msgPlain)
+    SendMessageInternal(service, "me", message1)
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+def CreateMessage(sender, to, subject, msgHtml, msgPlain):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    raw = base64.urlsafe_b64encode(msg.as_bytes())
+    raw = raw.decode()
+    body = {'raw': raw}
+    return body
+
+def send_email(email_to):
+    to = email_to
+    sender="me"
+    subject="Email from Python"
+    msgHtml = "<h3>Hello from Python!</h3>"
+    msgPlain = ""
+    SendMessage(sender, to, subject, msgHtml, msgPlain)
+    print("email sent")
+
+mails = ["1asumeasume1@gmail.com", "1dambikdambik1@gmail.com", "novcap22@gmail.com"]
+
+for i in mails:
+    send_email(i)
